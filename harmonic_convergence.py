1ae61c38def23a1af293992591f4c055c628bc21
chantalgerth <chantal.gerth@gmx.de>
2024-05-13 22:13:31 +0200

    Update harmonic_convergence.py

--- harmonic_convergence.py
+++ harmonic_convergence.py
@@ -1,118 +1,112 @@
+"""
+Dieses Programm implementiert die Funktionen vorwärts_summation und kahan_summation,
+um die Partialsummen der harmonischen Reihe mittels Vorwärtssummation bzw. Kahan-Summation
+zu berechnen.
+
+pylint 2.16.2
+astroid 2.14.2
+Python 3.11.7 | packaged by Anaconda, Inc.
+| (main, Dec 15 2023, 18:05:47) [MSC v.1916 64 bit (AMD64)]
+
+"""
+
 import numpy as np
 from py_logspace import py_logspace
 
-def vorwärts_summation(start, stop, num, basis, data_type):
-    
-    #Indexliste der Folgeglieder
-    liste = py_logspace(start, stop, num, basis)
+def vorwaerts_summation(start, stop, num, basis, data_type) -> list:
+    """
+    Berechnet die Partialsummen der harmonischen Reihe mittels Vorwärtssummation.
+
+    Inputs:
+        start (int): Der Anfangswert für den Logarithmusraum.
+        stop (int): Der Endwert für den Logarithmusraum.
+        num (int): Die Anzahl der Werte im Logarithmusraum.
+        basis (int): Die Basis des Logarithmusraums.
+        data_type (numpy.dtype): Der Datentyp für die Berechnung.
+
+    Throws:
+        ValueError: Wenn ungültige Eingaben für start, stop, num oder basis gemacht werden.
+
+    Returns:
+        list: Eine Liste der berechneten Partialsummen.
+
+    """
     result = []
-    
-    #Berechnung
-    for i in range(num):
-        
-        k=liste[i]
-        
-        #Berechnung Folgeglieder
-        partialsumme=data_type(0)
-        x=0
-        while x<k:
-            partialsumme=partialsumme+data_type((1/(x+1)))
-            x += 1
+    for k in py_logspace(start, stop, num, basis):
+        partialsumme = data_type(0)
+        for variable in range(int(k)):
+            partialsumme += data_type(1 / (variable + 1))
         result.append(partialsumme)
-        
-    return(result)
+    return result
 
-def kahan_summation(start, stop, num, basis, data_type):
-    
-    #Indexliste der Folgeglieder
-    liste = py_logspace(start, stop, num, basis)
-    
-    #Berechnung
-    for i in range(num):
-        
-        k=liste[i]
-        
-        #Berechnung Folgeglieder
-        partialsumme=0
-        x=0
-        kompensation=0
-        while x<k:
-            
-            vorläufige_partialsumme=partialsumme+(1/(x+1))
-            
-            if abs(sum) >= abs(1/(x+1)):
-                kompensation += (partialsumme - vorläufige_partialsumme) + x
-                
+def kahan_summation(start, stop, num, basis, data_type) -> list:
+    """
+    Berechnet die Partialsummen der harmonischen Reihe mittels Kahan-Summation.
+
+    Inputs:
+        start (int): Der Anfangswert für den Logarithmusraum.
+        stop (int): Der Endwert für den Logarithmusraum.
+        num (int): Die Anzahl der Werte im Logarithmusraum.
+        basis (int): Die Basis des Logarithmusraums.
+        data_type (numpy.dtype): Der Datentyp für die Berechnung.
+
+    Throws:
+        ValueError: Wenn ungültige Eingaben für start, stop, num oder basis gemacht werden.
+
+    Returns:
+        list: Eine Liste der berechneten Partialsummen.
+
+    """
+    result = []
+    for k in py_logspace(start, stop, num, basis):
+        partialsumme = data_type(0)
+        kompensation = data_type(0)
+        for variable in range(int(k)):
+            vorläufige_partialsumme = partialsumme + data_type(1 / (variable + 1))
+            if abs(partialsumme) >= abs(data_type(1 / (variable + 1))):
+                kompensation += (partialsumme - vorläufige_partialsumme) + (1 / (variable+1))
             else:
-                kompensation += (x - vorläufige_partialsumme) + x
-            
-            partialsumme = vorläufige_partialsumme + kompensation
-            liste[i]=partialsumme
-    
-    return(liste)
-            
+                kompensation += ((1 / (variable+1)) - vorläufige_partialsumme) + partialsumme
+            partialsumme = vorläufige_partialsumme
+        result.append(partialsumme)
+    return result
+
 def main():
-    """Hauptfunktion des Programms"""
-
-    # Definition der Variablen
-    start = input("Start: ")
+    """
+    Hauptfunktion des Programms, die die Berechnung und Ausgabe der Partialsummen durchführt.
+    """
     while True:
         try:
-            start = int(start)
+            start = int(input("Start: "))
+            stop = int(input("Stop: "))
+            num = int(input("Num: "))
+            basis = int(input("Basis: "))
+
+            # Vorwaertssummation mit verschiedenen Datentypen
+            result_vorwaerts_float16 = vorwaerts_summation(start, stop, num, basis, np.float16)
+            print("Vorwaertssummation mit np.float16:", result_vorwaerts_float16)
+
+            result_vorwaerts_float32 = vorwaerts_summation(start, stop, num, basis, np.float32)
+            print("Vorwaertssummation mit np.float32:", result_vorwaerts_float32)
+
+            result_vorwaerts_float64 = vorwaerts_summation(start, stop, num, basis, np.float64)
+            print("Vorwaertssummation mit np.float64:", result_vorwaerts_float64)
+
+            # Kahan-Summation mit verschiedenen Datentypen
+            result_kahan_float16 = kahan_summation(start, stop, num, basis, np.float16)
+            print("Kahansummation mit np.float16:", result_kahan_float16)
+
+            result_kahan_float32 = kahan_summation(start, stop, num, basis, np.float32)
+            print("Kahansummation mit np.float32:", result_kahan_float32)
+
+            result_kahan_float64 = kahan_summation(start, stop, num, basis, np.float64)
+            print("Kahansummation mit np.float64:", result_kahan_float64)
+
             break
+
         except ValueError:
-            start = input("Bitte geben Sie eine ganze Zahl ein. Start: ")
-    
-    stop = input("Stop: ")
-    while True:
-        try:
-            stop = int(stop)
-            break
-        except ValueError:
-            stop = input("Bitte geben Sie eine ganze Zahl ein. Stop: ")
-
-    num = input("Num: ")
-    while True:
-        try:
-            num = int(num)
-            if num >= 2:
-                break
-            else:
-                num = input("Bitte geben Sie eine Zahl >= 2 ein. Num: ")
-        except ValueError:
-            num = input("Bitte geben Sie eine ganze Zahl ein. Num: ")
-    
-    basis = input("Basis: ")
-    while True:
-        try:
-            basis = int(basis)
-            break
-        except ValueError:
-            basis = input("Bitte geben Sie eine ganze Zahl ein. Basis: ")
-
-    try:
-        # Beispielaufrufe für die Funktionen
-        result_vorwärts_float16 = vorwärts_summation(np.float16)
-        print("Vorwärtssummation mit np.float16:", result_vorwärts_float16)
-
-        result_vorwärts_float32 = vorwärts_summation(np.float32)
-        print("Vorwärtssummation mit np.float32:", result_vorwärts_float32)
-
-        result_vorwärts_float64 = vorwärts_summation(np.float64)
-        print("Vorwärtssummation mit np.float64:", result_vorwärts_float64)
-
-        result_kahan_float16 = kahan_summation(np.float16)
-        print("Kahansummation mit np.float16:", result_kahan_float16)
-
-        result_kahan_float32 = kahan_summation(np.float32)
-        print("Kahansummation mit np.float32:", result_kahan_float32)
-
-        result_kahan_float64 = kahan_summation(np.float64)
-        print("Kahansummation mit np.float64:", result_kahan_float64)
-        
-    except ValueError:
-        print("Funktionsaufruf gescheitert. Bitte neu aufrufen.")
-      
+            print("Fehler beim Funktionsaufruf. Bitte überprüfen Sie die Eingabewerte.")
+            continue
 
 if __name__ == "__main__":
     main()
