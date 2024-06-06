ca030268b1e0cce76d8ebfadc6df91deb1b56187
chantalgerth <chantal.gerth@gmx.de>
2024-06-06 20:59:25 +0200

    Update approx_pi.py

--- MAP/approx_pi.py
+++ MAP/approx_pi.py
@@ -1,4 +1,5 @@
-import time
+#import time #Zeitmessung
+import random #Zufallszahlen
 
 def pi_leibniz(tol: float):
     pi = 4
@@ -10,13 +11,26 @@ def pi_leibniz(tol: float):
         sk = 8./(16* k * k - 1)
     return pi
 
+def pi_montecarlo(num_points):
+    
+    inside_circle = 0
+
+    for p in range(num_points):
+        x = random.uniform(-1,1)
+        y = random.uniform(-1,1)
+        if x*x + y*y <= 1:
+            inside_circle += 1
+    return (inside_circle / num_points) * 4
+
+
 def main():
     toleranz = float(input("Genauigkeit: "))
-    start=time.time()
     pi1 = pi_leibniz(toleranz)
-    ende = time.time()
-    print('{:5.10f}s'.format(ende-start))
     print(pi1)
+    
+    num_points=int(input("Anzahl der Punkte: "))
+    pi2=pi_montecarlo(num_points)
+    print(pi2)
 
 if __name__ == "__main__":
     main()
