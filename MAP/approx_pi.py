47ee7b3d48bb951eee7a1f06159363bd0c46cf4b
chantalgerth <chantal.gerth@gmx.de>
2024-06-11 21:39:31 +0200

    Es klappt jetzt besser!

--- MAP/approx_pi.py
+++ MAP/approx_pi.py
@@ -3,29 +3,32 @@ import time #Zeitmessung
 import random #Zufallszahlen
 from decimal import Decimal, getcontext #Datentyp
 
-def pi_leibniz(tol: Decimal) -> Decimal:
+def pi_leibniz(tol: int) -> Decimal:
+
     """
     Berechnet eine Näherung von Pi mit der Leibniz-Reihe.
 
     Inputs:
-    tol (Decimal): Toleranz oder Genauigkeit für die Berechnung.
+    tol (int): Toleranz oder Genauigkeit für die Berechnung.
 
     Returns:
     Decimal: Eine Näherung von Pi.
     """
+
     getcontext().prec = 50  # Setzt die Präzision für Decimal-Berechnungen
     pi_approx = Decimal(4)
-    sk = Decimal(8) / Decimal(15)
-    k = 1
+    a_n = Decimal(8) / Decimal(15)
+    n = 1
 
-    while sk >= tol:
-        pi_approx = pi_approx - sk
-        k += 1
-        sk = Decimal(8./(16* k*k - 1))
+    for k in range(tol-1):
+        pi_approx = pi_approx - a_n
+        n += 1
+        a_n = Decimal(8)/Decimal(16* n*n - 1)
         
     return pi_approx
 
 def pi_montecarlo(num_points: int) -> Decimal:
+ 
     """
     Berechnet eine Näherung von Pi mit der Monte-Carlo-Methode.
 
@@ -35,36 +38,37 @@ def pi_montecarlo(num_points: int) -> Decimal:
     Returns:
     Decimal: Eine Näherung von Pi.
     """
-    
+ 
     getcontext().prec = 50  # Setzt die Präzision für Decimal-Berechnungen
 
     inside_circle = 0
 
-    for i in range(num_points):
+    for i in range(num_points-1):
         
         x = Decimal(random.uniform(-1, 1))
         y = Decimal(random.uniform(-1, 1))
         if x * x + y * y <= 1:
             inside_circle += 1
-    return (Decimal(inside_circle) / Decimal(num_points)) * Decimal(4)
+    return (Decimal(inside_circle /num_points)) * Decimal(4)
 
-def pi_viete(tol: Decimal) -> Decimal:
+def pi_viete(tol: int) -> Decimal:
+
     """
     Berechnet eine Näherung von Pi mit dem Viète-Algorithmus.
 
     Inputs:
-    tol (Decimal): Toleranz oder Genauigkeit für die Berechnung.
+    tol (int): Toleranz oder Genauigkeit für die Berechnung.
 
     Returns:
     Decimal: Eine Näherung von Pi.
     """
-    getcontext().prec = 50  # Setzt die Präzision für Decimal-Berechnungen
-    a_n = Decimal(np.sqrt(2))
-    pi_approx = Decimal(2) / a_n
-    k = 1
 
-    for k in range (tol):
-        a_n = np.sqrt((Decimal(2) + a_n))
+    getcontext().prec = 50 # Setzt die Präzision für Decimal-Berechnungen
+    a_n = Decimal(np.sqrt(2))
+    pi_approx = Decimal(2) * Decimal(2/a_n)
+
+    for k in range (tol-1):
+        a_n = Decimal(np.sqrt(Decimal(2) + a_n))
         pi_approx = pi_approx * Decimal(2) / a_n
 
     return pi_approx
@@ -74,7 +78,7 @@ def main():
     """
     Hauptfunktion des Programms.
     """
-    toleranz = Decimal(input("Bitte die gewünschte Genauigkeit eingeben: "))
+    toleranz = int(input("Bitte die gewünschte Genauigkeit eingeben: "))
     pi_leibniz_approx = pi_leibniz(toleranz)
     print(f"Pi (Leibniz-Reihe): {pi_leibniz_approx}")
 
