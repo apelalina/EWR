32f7b07e343465d8ce1f0335f17dc6ee49a4a220
chantalgerth <chantal.gerth@gmx.de>
2024-06-17 15:41:15 +0200

    Update approx_pi.py

--- MAP/approx_pi.py
+++ MAP/approx_pi.py
@@ -25,6 +25,19 @@ def plot_pi(data, y = "Fehler"):
     
     plt.show()
 
+def error_pi(calculated_pi: Decimal) -> (Decimal):
+    
+    getcontext().prec = 1010
+    
+    pi = '3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446095505822317253594081284811174502841027019385211055596446229489549303819644288109756659334461284756482337867831652712019091456485669234603486104543266482133936072602491412737245870066063155881748815209209628292540917153643678925903600113305305488204665213841469519415116094330572703657595919530921861173819326117931051185480744623799627495673518857527248912279381830119491298336733624406566430860213949463952247371907021798609437027705392171762931767523846748184676694051320005681271452635608277857713427577896091736371787214684409012249534301465495853710507922796892589235420199561121290219608640344181598136297747713099605187072113499999983729780499510597317328160963185950244594553469083026425223082533446850352619311881710100031378387528865875332083814206171776691473035982534904287554687311595628638823537875937519577818577805321712268066130019278766111959092164201989'
+   
+    fehler = Decimal(pi) * Decimal('1') - calculated_pi
+    
+    if fehler < Decimal('0'):
+        fehler = Decimal('-1') * fehler
+    
+    return fehler
+
 def decimal_factorial(n):
    """
    Berechnet die Fakultät einer gegebenen Zahl n unter Verwendung des Decimal-Datentyps.
@@ -41,24 +54,11 @@ def decimal_factorial(n):
 
    #Berechnung der Fakultät mit Decimal
    for i in range(1, n + 1):
-       result *= Decimal(i)*Decimal('1') # * Decimal('1'), sonst wird Decimal(i) nicht korrekt dargestellt
+       result *= Decimal(i) * Decimal('1') 
 
    return result
 
-def error_pi(calculated_pi: Decimal) -> (Decimal):
-    
-    getcontext().prec = 1010
-    
-    pi = '3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446095505822317253594081284811174502841027019385211055596446229489549303819644288109756659334461284756482337867831652712019091456485669234603486104543266482133936072602491412737245870066063155881748815209209628292540917153643678925903600113305305488204665213841469519415116094330572703657595919530921861173819326117931051185480744623799627495673518857527248912279381830119491298336733624406566430860213949463952247371907021798609437027705392171762931767523846748184676694051320005681271452635608277857713427577896091736371787214684409012249534301465495853710507922796892589235420199561121290219608640344181598136297747713099605187072113499999983729780499510597317328160963185950244594553469083026425223082533446850352619311881710100031378387528865875332083814206171776691473035982534904287554687311595628638823537875937519577818577805321712268066130019278766111959092164201989'
-   
-    fehler = Decimal(pi) - calculated_pi
-    
-    if fehler < Decimal('0'):
-        fehler = Decimal('-1') * fehler
-    
-    return fehler
-
-def pi_leibniz(tol: int) -> (Decimal, int, float):
+def pi_leibniz(tol: int) -> Decimal:
     """
     Berechnet eine Näherung von Pi mit der Leibniz-Reihe.
 
@@ -81,27 +81,31 @@ def pi_leibniz(tol: int) -> (Decimal, int, float):
         pi_approx = pi_approx - a_n
         n += Decimal('1')
         a_n = Decimal('8') / (Decimal('16')* (Decimal(n)*Decimal('1')) * (Decimal(n)*Decimal('1')) - Decimal('1'))
-        operations += Decimal('6')
+        operations += Decimal('8')
     end_time = time.time()
 
-    elapsed_time = (end_time - start_time) * Decimal('1000')
+    elapsed_time = (Decimal(end_time) * Decimal('1') - Decimal(start_time) * Decimal('1')) * Decimal('1000') * Decimal('1')
     return pi_approx, operations, elapsed_time
 
-def pi_montecarlo(num_points) -> (Decimal, int, float):
+def pi_montecarlo(num_points) -> Decimal:
     
     inside_circle = Decimal('0')
     start_time = time.time()
+    operations = Decimal('0')
     for p in range(num_points):
         x = random.uniform(-1,1)
         y = random.uniform(-1,1)
         if x*x + y*y <= 1:
             inside_circle += Decimal('1')
+            operations =+ Decimal('5')
+    pi_approx = (inside_circle * Decimal('1')) / (num_points * Decimal('1')) * Decimal('4')
+    operations =+ Decimal('4')
     end_time = time.time()
-    elapsed_time = (end_time - start_time) * Decimal
-    return (inside_circle / num_points) * 4
+    elapsed_time = (Decimal(end_time) * Decimal('1') - Decimal(start_time) * Decimal('1')) * Decimal('1000') * Decimal('1')
+    return pi_approx, operations, elapsed_time
 
 
-def pi_viete(tol: int) -> (Decimal, int, float):
+def pi_viete(tol: int) -> Decimal:
     """
     Berechnet eine Näherung von Pi mit dem Viète-Algorithmus.
 
@@ -119,14 +123,14 @@ def pi_viete(tol: int) -> (Decimal, int, float):
     pi_approx = Decimal('2') * Decimal('2')/ a_n
     operations = Decimal('3')
 
-    for k in range(1,tol+1):
+    for k in range(2,tol+1):
         a=Decimal('2') + a_n
         a_n = Decimal(a).sqrt()
-        pi_approx = pi_approx * (Decimal(2) / a_n)
+        pi_approx = pi_approx * (Decimal('2') / a_n)
         operations += Decimal('4') 
     end_time = time.time()
 
-    elapsed_time = (end_time - start_time) * 1000
+    elapsed_time = (Decimal(end_time) * Decimal('1') - Decimal(start_time) * Decimal('1')) * Decimal('1000') * Decimal('1')
     return pi_approx, operations, elapsed_time
 
 def pi_chudnovsky(tol: int) -> (Decimal, int, float):
@@ -177,7 +181,15 @@ def main():
     print(f"Fehler (Leibniz-Reihe): {fehler:.100f} (100 Nachkommastellen)")
     print(f"Natürlicher Logarithmus des Fehlers (Leibniz-Reihe): {fehler.ln():.50f} (50 Nachkommastellen)")
 
-    toleranz = int(input("Bitte den gewünschten Index des Partialproduktes der Viete-Methode eingeben: "))
+    num_points = int(input("Bitte die Anzahl der Punkte für die Monte-Carlo-Methode eingeben: "))
+    pi_montecarlo_approx, montecarlo_ops, montecarlo_time = pi_montecarlo(num_points)
+    print(f"Pi (Monte-Carlo-Methode): {pi_montecarlo_approx}")
+    print(f"Anzahl der Operationen (Monte-Carlo-Methode): {montecarlo_ops}")
+    print(f"Benötigte Zeit (Monte-Carlo-Methode): {montecarlo_time:.6f} Millisekunden")
+    fehler = error_pi(pi_montecarlo_approx)
+    print(f"Fehler (Monte-Carlo-Algorithmus): {fehler:.100f} (100 Nachkommastellen)")
+    print(f"Natürlicher Logarithmus des Fehlers (Monte-Carlo-Algorithmus): {fehler.ln():.50f} (50 Nachkommastellen)")
+    
     pi_viete_approx, viete_ops, viete_time = pi_viete(toleranz)
     print(f"Pi (Viète-Algorithmus): {pi_viete_approx}")
     print(f"Anzahl der Operationen (Viète-Algorithmus): {viete_ops}")
